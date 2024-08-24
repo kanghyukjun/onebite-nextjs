4f4e7b82db354b09fe35cfad5c3cd43cdbf9b6e1
강혁준 <domohit5124@naver.com>
2024-08-25 01:38:27 +0900

    SSR 실습

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,21 +1,34 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allBooks, recommendedBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
+
+  return {
+    props: {
+      allBooks,
+      recommendedBooks,
+    },
+  };
+};
+
+export default function Home({ allBooks, recommendedBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recommendedBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
