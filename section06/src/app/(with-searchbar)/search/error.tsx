bf96c45b94ce5f9a93a0381670fc53f8e93faa2e
강혁준 <domohit5124@naver.com>
2024-09-03 21:09:09 +0900

    error

---
+++ section06/src/app/(with-searchbar)/search/error.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h3>값을 불러오는 중 에러가 발생했습니다</h3>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh();
+            reset();
+          });
+        }}
+      >
+        다시 시도
+      </button>
+    </div>
+  );
+}
