b110e1193801e5462f716df90e8c60d643ad9a44
강혁준 <domohit5124@naver.com>
2024-09-05 00:28:30 +0900

    server action revalidate

--- section07/src/action/write-content.action.ts
+++ section07/src/action/write-content.action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath, revalidateTag } from "next/cache";
+
 export async function writeContent(formData: FormData) {
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
@@ -9,15 +11,15 @@ export async function writeContent(formData: FormData) {
     return;
   }
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
-    {
-      method: "POST",
-      body: JSON.stringify({
-        content,
-        author,
-        bookId,
-      }),
-    }
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
+    method: "POST",
+    body: JSON.stringify({
+      content,
+      author,
+      bookId,
+    }),
+  });
+
+  // revalidatePath(`/book/${bookId}`); // 이 경로에 해당하는 페이지를 다시 생성한다
+  revalidateTag(`review-${bookId}`);
 }
